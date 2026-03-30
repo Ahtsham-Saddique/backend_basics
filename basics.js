1c056ef256de86de51ee4ee3ddbb70cf0cbfd846
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-30 09:40:46 +0500

    foreEach vs map

--- basics.js
+++ basics.js
@@ -8,3 +8,26 @@ console.log(fruits[1].Apple+"\n");
 console.log(fruits[2][1]+"\n");
 console.log(fruits[fruits.length-1]+"\n");
 
+
+// foreach-map-filter-find-indexof-slice-push-pop-shift-unshift-charat
+
+// forEach affect on original array
+fruits.forEach((val,index,arr)=>
+{
+
+  arr[index]=  val+"modified";
+});
+
+
+    console.log(fruits);
+
+
+    
+// map does not  affect on original array return copy of modified or same array
+
+
+let mapped_Fruits = fruits.map((val,index,arr)=>
+{
+    return arr[val]=val+"mapped";
+});
+console.log(mapped_Fruits);
