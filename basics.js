5099225681741bcd6a393c4e091cd879cced5506
Ahtsham-Saddique <ahtshamsaddique55@gmail.com>
2026-03-30 09:24:06 +0500

    array concepts

---
+++ basics.js
@@ -0,0 +1,10 @@
+// Array
+
+let fruits = [ "Banana", {Apple : 3}, ["orange","kenno"],function veg(){
+
+}]
+
+console.log(fruits[1].Apple+"\n");
+console.log(fruits[2][1]+"\n");
+console.log(fruits[fruits.length-1]+"\n");
+
